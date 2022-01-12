e9af22ce43890895345dab41edae15ab34a42df0
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-01-12 11:14:44 -0500

    commit tree e4ff470083a9015c9518674613ff413d529213a4

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (someNumber <= 2499) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500) {
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(destinationCity) {
+  let travelAdvisery = destinationCity === 'NYC'? 'Ok, sounds good.' : 'No go.';
+  return travelAdvisery;
+}
+ternaryCheckCity('NYC')
+
+
+function switchOnCharmFromTip(tipAmount) {
   // Write your code here!
+  let tipResponse
+  switch (tipAmount) {
+    case 'generous':
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = 'Thank you.';
+      break;
+    default:
+      tipResponse = 'Bye.';
+  }
+  return tipResponse
 }
